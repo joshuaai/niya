ddd0997380f6a5e568d251fa323f39cfc9d3ae42
josoftme <joshuaiheji@gmail.com>
2017-02-25 16:19:19 +0000

    Set config.serve_static_assets = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Disable Rails's static asset server (Apache or nginx will already do this)  
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
