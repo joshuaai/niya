d2bc637e841fce60209cba1873b02ba8c9d6b8ba
josoftme <joshuaiheji@gmail.com>
2017-02-28 14:00:04 +0000

    Drop and recreate the venues table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228110502) do
+ActiveRecord::Schema.define(version: 20170228135901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20170228110502) do
   create_table "venues", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
-    t.integer  "size"
+    t.string   "size"
     t.string   "featured_image"
     t.integer  "user_id"
     t.integer  "price"
